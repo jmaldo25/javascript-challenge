f7f8efafd09eba2bddc90402f6f496c37a9788dc
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-06-28 23:14:50 -0500

    added loop for js data

--- static/js/app.js
+++ static/js/app.js
@@ -9,4 +9,12 @@ var tbody = d3.select("tbody");
 // console.log(data);
 
 // Create loop for data and create table update w/ data js file
+tableData.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
 
