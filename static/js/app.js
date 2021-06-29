586ec4d0f772bd58cd9fb1a985faa93a2d1dde98
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-06-29 15:11:39 -0500

    added event handlers in js

--- static/js/app.js
+++ static/js/app.js
@@ -21,3 +21,20 @@ tableData.forEach((ufoReport) => {
 var button = d3.select("#filter-btn")
 
 var resetButton = d3.select("#reset-filter-btn")
+
+// Set up event handlers
+button.on("click", function() {
+
+    // Set up for refresh by removing any previous table
+    d3.select("tbody").html("");
+
+    // Prevent page refresh
+    d3.event.preventDefault();
+
+    // Set up the select for the table and set to lowercase
+    var date = d3.select("#datetime").property("value");
+    var city = d3.select("#city").property("value").toLowerCase();
+    var state = d3.select("#state").property("value").toLowerCase();
+    var country = d3.select("#country").property("value").toLowerCase();
+    var shape = d3.select("#shape").property("value").toLowerCase();
+})
