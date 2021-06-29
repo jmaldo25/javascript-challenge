f4edbfd513f62cef644075dba39367f28a8c9324
Josue Maldonado <joshmaldo@Josues-MacBook-Pro.local>
2021-06-28 23:07:00 -0500

    starting js code for table creation

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,12 @@
+// Level 1 & Level 2 - combined for multiple search criteria to filter table
+
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Reference to the table body in html
+var tbody = d3.select("tbody");
+
+// console.log(data);
+
+// Create loop for data and create table update w/ data js file
+
